dc8ee39ed14a5b9ae6d4d970ff4849c9a2673481
玉玲 刘 <2975088785@qq.com>
2024-09-20 16:58:25 +0800

    121

--- CarProject/HomeIndex/HomeIndex.Designer.cs
+++ CarProject/HomeIndex/HomeIndex.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeIndex));
+            this.uiTabControlMenu1 = new Sunny.UI.UITabControlMenu();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.uiPanel1 = new Sunny.UI.UIPanel();
+            this.uiDigitalLabel1 = new Sunny.UI.UIDigitalLabel();
+            this.uiTabControlMenu1.SuspendLayout();
+            this.uiPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // uiTabControlMenu1
+            // 
+            this.uiTabControlMenu1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.uiTabControlMenu1.Controls.Add(this.tabPage1);
+            this.uiTabControlMenu1.Controls.Add(this.tabPage2);
+            this.uiTabControlMenu1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.uiTabControlMenu1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.uiTabControlMenu1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTabControlMenu1.Location = new System.Drawing.Point(0, 64);
+            this.uiTabControlMenu1.Multiline = true;
+            this.uiTabControlMenu1.Name = "uiTabControlMenu1";
+            this.uiTabControlMenu1.SelectedIndex = 0;
+            this.uiTabControlMenu1.Size = new System.Drawing.Size(1113, 578);
+            this.uiTabControlMenu1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.uiTabControlMenu1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(201, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(912, 578);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(201, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(912, 572);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // uiPanel1
+            // 
+            this.uiPanel1.Controls.Add(this.uiDigitalLabel1);
+            this.uiPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.uiPanel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiPanel1.Location = new System.Drawing.Point(0, 0);
+            this.uiPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiPanel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiPanel1.Name = "uiPanel1";
+            this.uiPanel1.Size = new System.Drawing.Size(1113, 61);
+            this.uiPanel1.TabIndex = 1;
+            this.uiPanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uiDigitalLabel1
+            // 
+            this.uiDigitalLabel1.BackColor = System.Drawing.Color.Black;
+            this.uiDigitalLabel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiDigitalLabel1.ForeColor = System.Drawing.Color.Lime;
+            this.uiDigitalLabel1.Location = new System.Drawing.Point(3, 12);
+            this.uiDigitalLabel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiDigitalLabel1.Name = "uiDigitalLabel1";
+            this.uiDigitalLabel1.Size = new System.Drawing.Size(151, 42);
+            this.uiDigitalLabel1.TabIndex = 0;
+            this.uiDigitalLabel1.Text = "uiDigitalLabel1";
             // 
             // HomeIndex
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1113, 642);
+            this.Controls.Add(this.uiPanel1);
+            this.Controls.Add(this.uiTabControlMenu1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "HomeIndex";
-            this.Text = "HomeIndex";
+            this.Text = "首页";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HomeIndex_FormClosed);
+            this.uiTabControlMenu1.ResumeLayout(false);
+            this.uiPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Sunny.UI.UITabControlMenu uiTabControlMenu1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private Sunny.UI.UIPanel uiPanel1;
+        private Sunny.UI.UIDigitalLabel uiDigitalLabel1;
     }
 }
