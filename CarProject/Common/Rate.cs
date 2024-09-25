bc8e7173fd1f2867ba3fc4d0d2741035105ea39b
玉玲 刘 <2975088785@qq.com>
2024-09-25 14:37:27 +0800

    12

---
+++ CarProject/Common/Rate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Method
+{
+    /// <summary>
+    /// 网速
+    /// </summary>
+    public struct Rate
+    {
+        public Rate(DateTime startTime, long receivedLength, long sendLength)
+        {
+            StartTime = startTime;
+            ReceivedLength = receivedLength;
+            SendLength = sendLength;
+        }
+
+        /// <summary>
+        /// 记录时间
+        /// </summary>
+        public DateTime StartTime { get; private set; }
+
+        /// <summary>
+        /// 此网卡总接收网络流量字节数
+        /// </summary>
+        public long ReceivedLength { get; private set; }
+
+        /// <summary>
+        /// 此网卡总发送网络流量字节数
+        /// </summary>
+        public long SendLength { get; private set; }
+    }
+
+}
---
+++ CarProject/Common/UnitType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Method
+{/// <summary>
+ /// 单位
+ /// </summary>
+    public enum UnitType : int
+    {
+        /// <summary>
+        /// Byte
+        /// </summary>
+        /// 
+        B = 0,
+        /// <summary>
+        /// KB
+        /// </summary>
+        KB,
+
+        /// <summary>
+        /// MB
+        /// </summary>
+        MB,
+
+        /// <summary>
+        /// GB
+        /// </summary>
+        GB,
+
+        /// <summary>
+        /// TB
+        /// </summary>
+        TB,
+
+        /// <summary>
+        /// PB
+        /// </summary>
+        PB
+    } 
+}
